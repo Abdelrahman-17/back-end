e0b015ddc6c447027c9756a60e8f586e6f0d7faf
Abdelrahman awad <83679041+Abdelrahman-17@users.noreply.github.com>
2022-10-09 17:42:28 +0200

    Delete counter.php

--- php/counter.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (empty($_SESSION['counter'])){
-    $_SESSION['counter']=1;
-}else{
-    $_SESSION['counter']++;
-}
-
-echo '<div> you viewed this is page '. $_SESSION['counter'].' times</div>';
